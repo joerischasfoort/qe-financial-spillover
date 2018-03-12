5cc6cb22ee18ddca91c362464a08824228a9248b
joerischasfoort <joerischasfoort@gmail.com>
2018-03-12 12:20:30 +0100

    added realised profits and returns functions + tests

--- functions/realised_returns.py
+++ functions/realised_returns.py
@@ -16,6 +16,27 @@ def hypothetical_asset_returns(fund, prices_tau, fx_rates):
                                                       fx_rates.loc[fund.par.country][asset.par.country])
 
 
+def realised_returns_asset(realised_profit, price, exchange_rate=1):
+    """
+    Equation 1.5 Calculate realised return on an asset
+    :param realised_profit: float realised profit on asset
+    :param price: float current asset price
+    :param exchange_rate: float current exchange rate (1 for domestic)
+    :return: float realised return on the asset
+    """
+    return realised_profit / (exchange_rate * price)
+
+
+def realised_return_cash(realised_profit, exchange_rate=1):
+    """
+    Calculate realised return on cash
+    :param realised_profit: float realised profit on currency
+    :param exchange_rate: float current exchange rate (1 for domestic)
+    :return: float realised return on cash
+    """
+    return realised_profit / exchange_rate
+
+
 def realised_profits_asset(default_rate, face_value, previous_price, price, quantity,
                            interest_rate, maturity, previous_exchange_rate=1, exchange_rate=1):
     """
@@ -24,7 +45,7 @@ def realised_profits_asset(default_rate, face_value, previous_price, price, quan
     :param face_value: float Face value
     :param previous_price: float Last price
     :param price: float current price
-    :param quantity: float asset quantity #TODO average or last? total? but then also total face value?
+    :param quantity: float total asset quantity
     :param interest_rate: float nominal interest rate
     :param maturity: float maturity
     :param previous_exchange_rate: float exchange rate calculated as previous_exchange_rate^{FD}= 1 / previous_exchange_rate^{DF}, standard no exchange rate previous_exchange_rate = 1
@@ -42,7 +63,7 @@ def realised_profits_asset(default_rate, face_value, previous_price, price, quan
     return realised_profit
 
 
-def realised_profits_cash(interest_rate, previous_exchange_rate, exchange_rate):
+def realised_profits_cash(interest_rate, previous_exchange_rate=1, exchange_rate=1):
     """
     Equations 1.3 Calculate realised returns on currency, if home currency, exchange rates are 1 and realised profits
     are equal to the interest rate.
--- functions/tests/test_realised_returns.py
+++ functions/tests/test_realised_returns.py
@@ -14,18 +14,42 @@ def init_params():
     maturity = 1
     return default_probability, previous_price, face_value, quantity, price, interest_rate, maturity
 
-def test_realised_profits_domestic(init_params):
+
+def test_realised_profits_assets(init_params):
     # if default prob = 0, maturity = 1, P_tau = P, and face_value = P*Q, return is interest rate & m = 1
     default_probability, previous_price, face_value, quantity, price, interest_rate, maturity = init_params
     assert_almost_equal(realised_profits_asset(default_probability, face_value,
                                                previous_price, price, quantity,
                                                interest_rate, maturity), interest_rate, 4)
-
-
-def test_realised_profits_foreign(init_params):
-    default_probability, previous_price, face_value, quantity, price, interest_rate, maturity = init_params
     # with an exchange rate of 1 this should be equal to the previous test
     assert_almost_equal(realised_profits_asset(default_probability, face_value,
                                                previous_price, price, quantity,
                                                interest_rate, maturity, exchange_rate=1,
                                                previous_exchange_rate=1), interest_rate, 4)
+
+
+def test_realised_profits_cash():
+    # for domestic assets the return is equal to the interest rate
+    interest_rate = 0.004
+    assert_almost_equal(realised_profits_cash(interest_rate, 1.0, 1.0), interest_rate, 4)
+    # when the interest rate is zero this still applies
+    interest_rate = 0.0
+    assert_almost_equal(realised_profits_cash(interest_rate, 1.0, 1.0), interest_rate, 4)
+
+
+def test_realised_return_cash():
+    """Test realised returns on cash function"""
+    interest_rate = 0.0
+    assert_almost_equal(realised_return_cash(interest_rate), interest_rate, 3)
+    # if the exchange rate is 2 the interest rate should be halved
+    interest_rate = 0.5
+    assert_almost_equal(realised_return_cash(interest_rate, exchange_rate=2), interest_rate / 2.0, 3)
+
+
+def test_realised_return():
+    """Test realised returns on asset function"""
+    realised_profit = 0.5
+    price = 1.0
+    assert_almost_equal(realised_returns_asset(realised_profit, price, exchange_rate = 1), 0.5, 3)
+    # if the exchange rate doubles, the returns half
+    assert_almost_equal(realised_returns_asset(realised_profit, price, exchange_rate=2), 0.25, 3)
