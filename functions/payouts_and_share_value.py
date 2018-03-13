209a969852ba626be88dfd1f693c2170e9f83ca3
joerischasfoort <joerischasfoort@gmail.com>
2018-03-13 09:47:12 +0100

    update shares value functions

--- functions/payouts_and_share_value.py
+++ functions/payouts_and_share_value.py
@@ -1,22 +1,39 @@
-from __future__ import division
+def payout_to_shareholders(fund):
+    # calculate current share target
+    shares_target_value = 1
+    # obtain previous share value
+    previous_shares_value = 1
+    # obtain realised profits
+    profits = 1
+    # calculate wanted shares value
+    wanted_shares_value = wanted_value_shares(shares_target_value, previous_shares_value, profits)
+    current_shares_value = 1 # TODO sum of assets + currencies
+    # calculate current value of redeemable shares
+    payouts = payout(current_shares_value, wanted_shares_value)
+    redeemable_shares = current_shares_value - payouts
+    return redeemable_shares
 
-def payouts_and_share_value(portfolios, currencies, fund, environment):
-    payout = {}
-    valuation_change = {}
-    repayment_effect = {}
-    price_effect = {}
-    for a in portfolios:
-        payout[a] = environment.var.fx_rates.loc[fund.par.country,a.par.country] * fund.var_previous.assets[a] * ((1-a.var.default_rate) * (a.par.face_value / a.par.quantity) * a.par.nominal_interest_rate - a.var.default_rate * a.var.price)
-        repayment_effect[a] = (1-a.par.maturity) * (environment.var.fx_rates.loc[fund.par.country,a.par.country] * (a.par.face_value / a.par.quantity) - environment.var_previous.fx_rates.loc[fund.par.country,a.par.country] * a.var_previous.price)
-        price_effect[a] = a.par.maturity * (environment.var.fx_rates.loc[fund.par.country,a.par.country] * a.var.price - environment.var_previous.fx_rates.loc[fund.par.country,a.par.country] * a.var_previous.price)
-        valuation_change[a] = fund.var_previous.assets[a] * (1- a.var.default_rate) * (repayment_effect[a] + price_effect[a])
-        
-    for c in currencies:
-        payout[c] = environment.var.fx_rates.loc[fund.par.country,c.par.country] * fund.var_previous.currency[c] * c.par.nominal_interest_rate
-        valuation_change[c] = fund.var.currency[c] * (environment.var.fx_rates.loc[fund.par.country,c.par.country] - environment.var.fx_rates.loc[fund.par.country,c.par.country])
-    
 
-        
-    redeemable_shares =  fund.var.redeemable_shares + sum(valuation_change.values())   
-    
-    return redeemable_shares, sum(payout.values())
+def wanted_value_shares(shares_target_value, previous_shares_value, profits):
+    """
+    Equation 1.4 the wanted share value for a fund
+    :param shares_target_value: float target value of shares
+    :param previous_shares_value: float previous actual value of shares
+    :param profits: float this periods profits
+    :return: float the desired level of shares
+    """
+    wanted_shares_value = min(shares_target_value, previous_shares_value + profits)
+    return wanted_shares_value
+
+
+def payout(shares_value, wanted_shares_value):
+    """
+    Given the wanted shares value the fund pays out, or asks share holders to commit extra funds
+    :param shares_value: float current value of shares
+    :param wanted_shares_value: float the desired value of shares
+    :return: float payout to shareholders, when negative this amounts to a bail-in by shareholders
+    """
+    payout = shares_value - wanted_shares_value
+    return payout
+
+
