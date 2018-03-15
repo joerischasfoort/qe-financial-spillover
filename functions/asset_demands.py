d808b73c7105334b9aed7769e90768f12cb4f147
jriedler <jesper_riedler@hotmail.com>
2018-03-15 14:48:25 +0200

    bugs, bugs, bugs

--- functions/asset_demands.py
+++ functions/asset_demands.py
@@ -24,16 +24,16 @@ def asset_demand(fund, portfolios, currencies, environment):
 
 
 def cash_inventory(fund, portfolios, currencies):
-    
+    cash_inv = {}
     for c in currencies:
         cash_inv[c] = fund.var_previous.currency[c] * (1 + c.par.nominal_interest_rate)
         
         for a in portfolios:
-            if a.par.country == fund.par.country
-            mat= (1-a.par.maturity) * (1 - a.var.default_rate)
-            all = (1 - a.var.default_rate)
-            cash_inv[c] = cash_inv[c] + (mat + all * a.par.nominal_interest_rate) * fund.var.assets[a] * a.par.face_value / a.par.quantity
-            
+            if a.par.country == fund.par.country:
+                mat= (1-a.par.maturity) * (1 - a.var.default_rate)
+                all = (1 - a.var.default_rate)
+                cash_inv[c] = cash_inv[c] + (mat + all * a.par.nominal_interest_rate) * fund.var.assets[a] * a.par.face_value / a.par.quantity
+                
         if c.par.country == fund.par.country:
             cash_inv[c] = cash_inv[c] + fund.var.payouts
     
