6422d8541f25216baa89be912baeeddd21f0e317
jriedler <jesper_riedler@hotmail.com>
2018-03-27 17:05:41 +0200

    cleaning

--- spillover_model.py
+++ spillover_model.py
@@ -107,40 +107,6 @@ def spillover_model(portfolios, currencies, environment, exogeneous_agents, fund
                 # compute demand for balance sheet positions
                 fund.var.asset_demand, fund.var.currency_demand = asset_demand(fund, portfolios, currencies, environment)
 
-                c0_t = fund.var_previous.currency[currencies[0]]
-                c1_t = fund.var_previous.currency[currencies[1]]
-                rhoC0 = currencies[0].par.nominal_interest_rate
-                rhoC1 = currencies[1].par.nominal_interest_rate
-                rho0 = portfolios[0].par.nominal_interest_rate
-                rho1 = portfolios[1].par.nominal_interest_rate
-                Q0_t = fund.var_previous.assets[portfolios[0]]
-                Q1_t = fund.var_previous.assets[portfolios[1]]
-                fxC0_t1 = environment.var.fx_rates.loc[fund.par.country][currencies[0].par.country]
-                fxC1_t1 = environment.var.fx_rates.loc[fund.par.country][currencies[1].par.country]
-                fxC0_t = environment.var_previous.fx_rates.loc[fund.par.country][currencies[0].par.country]
-                fxC1_t = environment.var_previous.fx_rates.loc[fund.par.country][currencies[1].par.country]
-                fx0_t1 = environment.var.fx_rates.loc[fund.par.country][portfolios[0].par.country]
-                fx1_t1 = environment.var.fx_rates.loc[fund.par.country][portfolios[1].par.country]
-                fx0_t = environment.var_previous.fx_rates.loc[fund.par.country][portfolios[0].par.country]
-                fx1_t = environment.var_previous.fx_rates.loc[fund.par.country][portfolios[1].par.country]
-                p0_t1 = portfolios[0].var.price
-                p1_t1 = portfolios[1].var.price
-                p0_t = portfolios[0].var_previous.price
-                p1_t = portfolios[1].var_previous.price
-                
-                dC0 = fxC0_t1*(c0_t*(1+rhoC0)+rho0*Q0_t)-c0_t*fxC0_t
-                dC1 = fxC1_t1*(c1_t*(1+rhoC1)+rho1*Q1_t)-c1_t*fxC1_t
-
-                d0 = Q0_t*(fx0_t1*p0_t1-fx0_t*p0_t)
-                d1 = Q1_t*(fx1_t1*p1_t1-fx1_t*p1_t)
-                
-                
-                
-                newA = Q0_t*p0_t1*fx0_t1+Q1_t*p1_t1*fx1_t1
-                newC = fund.var.currency_inventory[currencies[0]]*fxC0_t1 + fund.var.currency_inventory[currencies[1]]*fxC1_t1
-
-                #print "asset side effect:", fund, newA+newC-fund.var.redeemable_shares
-
             
             for ex in exogeneous_agents:
                 exogeneous_agents[ex].var.asset_demand = ex_agent_asset_demand(ex, exogeneous_agents, portfolios )
