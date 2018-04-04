1b5ff1df27042bda6ed55ea529cd27ea0c12e3f7
jriedler <jesper_riedler@hotmail.com>
2018-04-04 10:31:23 +0200

    minor errors

--- functions/profits_and_payouts.py
+++ functions/profits_and_payouts.py
@@ -43,7 +43,6 @@ def profit_and_payout(fund, portfolios, currencies, environment):
 
     for c in currencies:
         profit_per_asset[c] = c.par.nominal_interest_rate * environment.var.fx_rates.loc[fund.par.country, c.par.country] + environment.var.fx_rates.loc[fund.par.country, c.par.country] - environment.var_previous.fx_rates.loc[fund.par.country, c.par.country]
-        testing[c] = fund.var.currency[c] * profit_per_asset[c]
         total_payouts[c] = (total_payouts[c] + fund.var.currency[c] * c.par.nominal_interest_rate)
         #total_payouts[c] =0
         # the balance sheet effect takes into account the exchange rate effect
--- spillover_model.py
+++ spillover_model.py
@@ -106,7 +106,7 @@ def spillover_model(portfolios, currencies, environment, exogeneous_agents, fund
 
             Deltas = {}
             Deltas.update(Delta_Demand)
-            Deltas.update({"FX": Delta_Capital})
+            Deltas.update({"Delta_FX": Delta_Capital})
 
             convergence = sum(abs(Deltas[i])<0.00000001 for i in Deltas)==len(Deltas) and tau >30
 
