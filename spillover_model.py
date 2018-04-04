37ef1406c7b462741248a25ee6ec92761e9e0a0a
jriedler <jesper_riedler@hotmail.com>
2018-04-04 10:23:18 +0200

    testing market clearing

--- spillover_model.py
+++ spillover_model.py
@@ -103,7 +103,7 @@ def spillover_model(portfolios, currencies, environment, exogeneous_agents, fund
             Deltas = {}
             Deltas.update(Delta_Demand)
             Deltas.update({"FX": Delta_Capital})
-            convergence = sum(abs(Deltas[i])<0.0001 for i in Deltas)==len(Deltas) and tau >30
+            convergence = sum(abs(Deltas[i])<0.00000001 for i in Deltas)==len(Deltas) and tau >30
 
             print tau, convergence, Deltas
             #Update intraday data points
